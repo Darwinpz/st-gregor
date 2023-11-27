0c002ff107e7b7f6f7c19eb8cc97284df78fdb70
Darwinpz <dpilaloazea@gmail.com>
2023-11-27 10:12:41 -0500

    Update recetas.php

--- resources/views/pages/recetas.php
+++ resources/views/pages/recetas.php
@@ -10,7 +10,7 @@
                 
                 <div class="card-body">
                 
-                    <h3 class="card-title text-center text-white">Recetas</h3>
+                    <h3 class="card-title text-center text-white">Blog de Recetas</h3>
                 
                 </div>
                             
@@ -20,6 +20,146 @@
 
     </div>
 
+    <div class="row mt-2">
+
+        <div class="col col-12 col-sm-12 col-md-6 col-lg-4 d-flex mb-2">
+
+            <div class="card" style="border-color:#B88F23;border-width:medium;">
+
+                <a data-fslightbox href="<?php echo RUTA_URL;?>/img/recetas_ajillo.jpg">
+                    <img class="card-img-top" src="<?php echo RUTA_URL;?>/img/recetas_ajillo.jpg" class="img-fluid">
+                </a>
+
+                <div class="card-body">
+                    <h5 class="text-center"><b><span style="color:#B88F23">CAMARONES AL AJILLO</span></b></h5>
+                    <p style="font-size:18px;"><b>Ingredientes:</b></p>
+                    <ul>
+                        <li>1 Libra de Camarones</li>
+                        <li>3 Cucharadas de Mantequilla</li>
+                        <li>1 Cucharada de Aceite</li>
+                        <li>12 Dientes de Ajos</li>
+                        <li>½ Cebolla Perla</li>
+                        <li>1 Taza de Vino Blanco</li>
+                        <li>1 Taza de Crema</li>
+                        <li>Sal – Pimienta</li>
+                    </ul>
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="col col-12 col-sm-12 col-md-6 col-lg-4 d-flex mb-2">
+
+            <div class="card" style="border-color:#B88F23;border-width:medium;">
+
+                <a data-fslightbox href="<?php echo RUTA_URL;?>/img/recetas_ceviche.jpg">
+                    <img src="<?php echo RUTA_URL;?>/img/recetas_ceviche.jpg"  class="img-fluid">
+                </a>
+
+                <div class="card-body">
+                    <h5 class="text-center"><b><span style="color:#B88F23">CEVICHE DE CAMARÓN</span></b></h5>
+                    <p style="font-size:18px;"><b>Ingredientes:</b></p>
+                    <ul>
+                        <li>3 Libras de Camarón Entero</li>
+                        <li>2 Cebollas Coloradas</li>
+                        <li>3 Tomates</li>
+                        <li>8 Limones</li>
+                        <li>Cilantro o Perejil</li>
+                        <li>Sal - Pimienta</li>
+                    </ul>
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="col col-12 col-sm-12 col-md-6 col-lg-4 d-flex mb-2">
+
+            <div class="card" style="border-color:#B88F23;border-width:medium;">
+
+                <a data-fslightbox href="<?php echo RUTA_URL;?>/img/recetas_reventados.jpg">
+                    <img src="<?php echo RUTA_URL;?>/img/recetas_reventados.jpg" class="img-fluid">
+                </a>
+
+                <div class="card-body">
+                    <h5 class="text-center"><b><span style="color:#B88F23">CAMARONES REVENTADOS</span></b></h5>
+                    <p style="font-size:18px;"><b>Ingredientes:</b></p>
+                    <ul>
+                        <li>Camarones 500 gr</li>
+                        <li>Harina 4 tazas</li>
+                        <li>Sal 50 gr</li>
+                        <li>Pimienta molida 30 gr</li>
+                        <li>Comino 60 gr</li>
+                        <li>Cebolla en polvo 40 gr</li>
+                        <li>Paprika 40 gr</li>
+                        <li>Agua C/N</li>
+                        <li>Salsa de tomate 1/2 taza</li>
+                        <li>Mayonesa 1/2 taza</li>
+                        <li>Sal</li>
+                        <li>Pimienta</li>
+                    </ul>
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="col col-12 col-sm-12 col-md-6 col-lg-4 d-flex mb-2">
+
+            <div class="card" style="border-color:#B88F23;border-width:medium;">
+
+                <a data-fslightbox href="<?php echo RUTA_URL;?>/img/recetas_ceviche.jpg">
+                    <img src="<?php echo RUTA_URL;?>/img/recetas_ceviche.jpg" class="img-fluid">
+                </a>
+
+                <div class="card-body">
+                    <h5 class="text-center"><b><span style="color:#B88F23">TALLARÍN SALTADO CON CAMARONES</span></b></h5>
+                    <p style="font-size:18px;"><b>Ingredientes:</b></p>
+                    <ul>
+                        <li>Camarones 1 libra</li>
+                        <li>Fideos 400 Gramos</li>
+                        <li>Ajo Picado 1 Cucharada</li>
+                        <li>Jengibre Picado 1 Cucharada</li>
+                        <li>Brócoli 1 Taza</li>
+                        <li>Zanahoria 1 Unidad</li>
+                        <li>Pimiento Rojo ½ Unidad</li>
+                        <li>Pimiento Amarillo ½ Unidad</li>
+                        <li>Pimiento Verde 1 Unidad</li>
+                        <li>Cebolla Perla ½ Unidad</li>
+                        <li>Nabo ½ Taza</li>
+                    </ul>
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="col col-12 col-sm-12 col-md-6 col-lg-4 d-flex mb-2">
+
+            <div class="card" style="border-color:#B88F23;border-width:medium;">
+
+                <a data-fslightbox href="<?php echo RUTA_URL;?>/img/recetas_apanados.jpg">
+                    <img src="<?php echo RUTA_URL;?>/img/recetas_apanados.jpg" class="img-fluid">
+                </a>
+
+                <div class="card-body">
+                    <h5 class="text-center"><b><span style="color:#B88F23">CAMARONES APANADOS</span></b></h5>
+                    <p style="font-size:18px;"><b>Ingredientes:</b></p>
+                    <ul>
+                        <li>Camarones ½ K</li>
+                        <li>Pasta de Ajo 2 cucharadas</li>
+                        <li>Harina 2 tazas</li>
+                        <li>Huevos 2 Ud</li>
+                        <li>Apanadura 2 tazas</li>
+                    </ul>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
 
 
 </div>
